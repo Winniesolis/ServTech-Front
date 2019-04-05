4d3e346262d453249d193cb8272df63b5bdbb392
WinnieSolis <winniesolis97@gmail.com>
2019-04-04 22:17:27 -0600

    Maquetado1

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ $(function(){
     $(window).scroll(function(){
         var winTop = $(window).scrollTop();
         if(winTop >= 30){
-            $("body").addClass("sticky-header display-none ");
+            $("body").addClass("sticky-header display-none");
         }else{
             $("body").removeClass("sticky-header display-none");
         }
